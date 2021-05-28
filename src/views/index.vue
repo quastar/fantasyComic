0fbaa66f18e381ed9069300c4f95a66be05ba55a
sunzhanfei <2370830056@qq.com>
2021-05-28 10:37:43 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" src='./index.ts'/>
+<style lang="less">
+@import url('./index.less');
+</style>
+<template>
+  <div class="main-home">
+    <im-home v-if="Page==='home'" :conversations="conversations" @switchPage="switchPage"></im-home>
+    <im-im  v-if="Page==='im'" :chosedConversation="chosedConversation" @switchPage="switchPage"></im-im>
+  </div>
+</template>
